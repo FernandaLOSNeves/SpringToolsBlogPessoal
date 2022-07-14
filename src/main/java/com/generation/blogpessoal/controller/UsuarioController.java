b9b878462efe0de3711da4007caaeb92f39fc7a6
Fernanda <fernandalosneves@gmail.com>
2022-07-14 14:32:29 -0300

    JUnit Test feito.

--- src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
+++ src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
@@ -1,11 +1,16 @@
 package com.generation.blogpessoal.controller;
 
+import java.util.List;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.blogpessoal.model.UserLogin;
 import com.generation.blogpessoal.model.Usuario;
+import com.generation.blogpessoal.repository.UsuarioRepository;
 import com.generation.blogpessoal.service.UsuarioService;
 
 @RestController
@@ -21,18 +27,47 @@ import com.generation.blogpessoal.service.UsuarioService;
 
 public class UsuarioController {
 	
+	  /**
+     * Faz uma injeção de dependência da classe de Serviço UsuarioService
+     * para ter acesso aos métodos do CRUD com regras de negócio
+     */
+	
 	@Autowired
 	private UsuarioService usuarioService;
 	
+	//ESSE PRIVATE ADD HOJE 14/07
+	@Autowired
+    private UsuarioRepository usuarioRepository;
+	
+	//TAMBÉM ESTAVA SEM OS GET, APENAS A PARTIR DO POST
+	@GetMapping("/all")
+    public ResponseEntity <List<Usuario>> getAll(){
+
+        return ResponseEntity.ok(usuarioRepository.findAll());
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+        return usuarioRepository.findById(id)
+            .map(resposta -> ResponseEntity.ok(resposta))
+            .orElse(ResponseEntity.notFound().build());
+    }
+    
 	@PostMapping ("/logar")
 	public ResponseEntity<UserLogin> Autentication (@RequestBody Optional <UserLogin> user){
 		return usuarioService.logarUsuario(user).map(resp -> ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
 	}
 	
-	@PostMapping("/cadastrar")
-	public ResponseEntity<Optional<Usuario>> Post(@RequestBody Usuario usuario){
-		return ResponseEntity.status(HttpStatus.CREATED)
-				.body(usuarioService.cadastrarUsuario(usuario));
-	}
+	//ATUALIZEI O POST CADASTRAR
+	// PARTE DIFERENTE DO VIDEO ATUALIZADA
+	
+    @PostMapping("/cadastrar")
+    public ResponseEntity<Usuario> postUsuario(@Valid @RequestBody Usuario usuario) {
+
+        return usuarioService.cadastrarUsuario(usuario)
+            .map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(resposta))
+            .orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+    }
 }
--- src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
@@ -1,8 +1,10 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.generation.blogpessoal.model.Usuario;
 
@@ -10,4 +12,6 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	
 	public Optional<Usuario> findByUsuario(String usuario);
 
+	//Método criado para a Sessão de testes
+	public List<Usuario> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 }
