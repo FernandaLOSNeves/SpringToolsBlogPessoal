b859be1f5e896cae2a579d8641be305c31822cf5
Fernanda <fernandalosneves@gmail.com>
2022-07-19 17:16:53 -0300

    C:/Program Files/Git/atualizar adicionado no Usuario Controller

--- src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
+++ src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -68,6 +69,12 @@ public class UsuarioController {
 
         return usuarioService.cadastrarUsuario(usuario)
             .map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(resposta))
+            .orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+    }
+    @PutMapping("/atualizar")
+    public ResponseEntity<Usuario> putUsuario(@Valid @RequestBody Usuario usuario){
+        return usuarioService.atualizarUsuario(usuario)
+            .map(resp -> ResponseEntity.status(HttpStatus.OK).body(resp))
             .orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
     }
 }
